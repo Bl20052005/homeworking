8aabb7ac54dcfb12bb3600940acf0d2849eb32d7
Bl20052005 <88120481+Bl20052005@users.noreply.github.com>
2025-02-15 21:02:44 -0800

    recent changes

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,46 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "@/lib/mongodb";
+import bcrypt from "bcryptjs";
+import { MongoClient } from "mongodb";
+
+export default NextAuth({
+  providers: [
+    // GoogleProvider({
+    //   clientId: process.env.GOOGLE_CLIENT_ID,
+    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    // }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const client = await clientPromise;
+        const db = client.db();
+
+        const user = await db.collection("users").findOne({ email: credentials.email });
+
+        if (!user || !(await bcrypt.compare(credentials.password, user.password))) {
+          throw new Error("Invalid credentials");
+        }
+
+        return { id: user._id, email: user.email };
+      },
+    }),
+  ],
+  adapter: MongoDBAdapter(clientPromise),
+  session: { strategy: "jwt" },
+  callbacks: {
+    async session({ session, token }) {
+      if (token) {
+        session.user.id = token.sub;
+      }
+      return session;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+});
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,37 @@
+"use client";
+
 import Image from "next/image";
+import {useState} from "react";
 import { Button } from "@mantine/core";
+import { EmbedPDF } from "@simplepdf/react-embed-pdf";
 
 export default function Home() {
+  const [file, setFile] = useState(null);
+  const handleFileChange = (event)=>{
+    const selectedFile = event.target.files[0];
+    if(selectedFile && selectedFile.type === "application/pdf") {
+      const fileUrl = URL.createObjectURL(selectedFile);
+      setFile(fileUrl);
+    }
+  };
+
   return (
     <main>
       <Button variant="filled" className="relative">
-        <div>hello!</div>
-        <input type="file" className="absolute h-full w-full top-0 left-0 z-10 opacity-0"></input>
+        <div>Insert your file here!</div>
+        <input
+          type="file"
+          className="absolute h-full w-full top-0 left-0 z-10 opacity-0"
+          onChange={handleFileChange}
+        ></input>
       </Button>
+
+      {file && file.type === "application/pdf" && (
+        <div style={{marginTop: 20}}>
+          <h3>PDF: </h3>
+          <EmbedPDF file={file}/>
+        </div>
+      )}
     </main>
   );
 }
