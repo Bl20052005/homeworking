950431e17b8a230309027e30aa83c1daca67fbef
Bl20052005 <88120481+Bl20052005@users.noreply.github.com>
2025-02-16 21:23:55 -0800

    attempt

---
+++ src/app/latexRender.js
@@ -0,0 +1,18 @@
+"use client";
+
+import katex from "katex";
+import { useEffect, useRef } from "react";
+
+function LatexRender({ text }) {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    if (ref) {
+      katex.render(text, ref.current);
+    }
+  }, [text]);
+
+  return <div ref={ref}></div>;
+}
+
+export default LatexRender;
