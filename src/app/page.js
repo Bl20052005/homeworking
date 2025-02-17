90b22830e08a87c9dc5c4ee6d0662185608dd2cd
NisuJT <tranjustin18@gmail.com>
2025-02-16 22:11:00 -0800

    implemented background, headers, and login page

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,17 @@
 "use client";
 
-import Image from "next/image";
+// import Image from "next/image";
 import {useState} from "react";
 import { Button } from "@mantine/core";
-import { EmbedPDF } from "@simplepdf/react-embed-pdf";
+import "./tabs.js"
+import { useRouter } from 'next/navigation'
+// import { EmbedPDF } from "@simplepdf/react-embed-pdf";
 
 export default function Home() {
   const [file, setFile] = useState(null);
+
+  const router = useRouter()
+
   const handleFileChange = (event)=>{
     const selectedFile = event.target.files[0];
     if(selectedFile && selectedFile.type === "application/pdf") {
@@ -16,22 +21,8 @@ export default function Home() {
   };
 
   return (
-    <main>
-      <Button variant="filled" className="relative">
-        <div>Insert your file here!</div>
-        <input
-          type="file"
-          className="absolute h-full w-full top-0 left-0 z-10 opacity-0"
-          onChange={handleFileChange}
-        ></input>
-      </Button>
-
-      {file && file.type === "application/pdf" && (
-        <div style={{marginTop: 20}}>
-          <h3>PDF: </h3>
-          <EmbedPDF file={file}/>
-        </div>
-      )}
-    </main>
+    <>
+      
+      </>
   );
 }
---
+++ src/app/tabs.js
@@ -0,0 +1,38 @@
+"use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function Header() {
+  const [activeTab, setActiveTab] = useState(0);
+  const router = useRouter();
+
+  return (
+    <>
+      <h1 className="title">HOMEWORKING</h1>
+      <div>
+        <div className="tabs">
+          <div className="tabs-header">
+            <button
+              className={activeTab == 0 ? "button-active" : "button-inactive"}
+              onClick={() => {
+                router.push("/");
+                setActiveTab(0);
+              }}
+            >
+              Home
+            </button>
+            <button
+              className={activeTab == 1 ? "button-active" : "button-inactive"}
+              onClick={() => {
+                router.push("/login");
+                setActiveTab(1);
+              }}
+            >
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
