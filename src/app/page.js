0defa9ba1141bdb6e5d95749897aa96893298e42
Roy Park <roydp@uci.edu>
2025-03-29 16:05:39 -0700

    Managed to connect backend and frontend

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import { useState } from "react";
+import {useState, useEffect} from "react";
 import { Button } from "@mantine/core";
     
 import "./tabs.js"
@@ -11,6 +11,15 @@ import LatexRender from "./latexRender";
 
 export default function Home() {
   const [file, setFile] = useState(null);
+  const [message, setMessage] = useState("This is a placeholder for the message being sent from backend.");
+
+  useEffect(()=>{
+    fetch("http://127.0.0.1:5000/home").then(
+      response=>response.json()
+    ).then((data)=>{
+      setMessage(data.message);
+    });
+  });
   const handleFileChange = (event) => {
     const selectedFile = event.target.files[0];
     if (selectedFile && selectedFile.type === "application/pdf") {
@@ -22,6 +31,9 @@ export default function Home() {
   return (
     <main>
       <LatexRender text="x = \frac{-b \pm \sqrt{b^2 - 4ac}}{2a}" />
+      <div>
+        <div>{message}</div>
+      </div>
     </main>
   );
 }
